20a2efe73c760c791a82a0765f793fdcbdf3493d
Joan in DC <joanrigdon@gmail.com>
2019-01-31 16:54:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,10 @@ def prime?(integer)
     FALSE
   end
   
-  myArray = Array(2..9999)
+  myArray = Array(2..(integer-1))
   list_of_remainders = []
   myArray.each do |i|
     result = integer % i 
-    if integer != i
       list_of_remainders.push(result)
     end
   end
