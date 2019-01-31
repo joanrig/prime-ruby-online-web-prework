c6b85f62e5df4bd580839d4ee773c2f86190bd8a
Joan in DC <joanrigdon@gmail.com>
2019-01-31 16:56:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,45 +1,45 @@
-def prime?(integer)
-  if integer < 1 
-    FALSE
-  end
-  
-  myArray = Array(2..(integer-1))
-  list_of_remainders = []
-  myArray.each do |i|
-    result = integer % i 
-    list_of_remainders.push(result)
-  end
-    
-  if list_of_remainders.include? 0
-    FALSE
-  else
-    TRUE
-  end
-end
-
-
-# # Add  code here!
 # def prime?(integer)
-#   if integer < 2
-#     return FALSE
+#   if integer < 1 
+#     FALSE
 #   end
-    
-#   myArray = Array (2..9999)
+  
+#   myArray = Array(2..(integer-1))
 #   list_of_remainders = []
 #   myArray.each do |i|
-#     result = integer % i
-#     if integer != i
-#       list_of_remainders.push result
-#     end
+#     result = integer % i 
+#     list_of_remainders.push(result)
 #   end
-  
+    
 #   if list_of_remainders.include? 0
-#     return FALSE
+#     FALSE
 #   else
-#     return TRUE
+#     TRUE
 #   end
-  
 # end
+
+
+# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return FALSE
+  end
+    
+  myArray = Array (2..9999)
+  list_of_remainders = []
+  myArray.each do |i|
+    result = integer % i
+    if integer != i
+      list_of_remainders.push result
+    end
+  end
+  
+  if list_of_remainders.include? 0
+    return FALSE
+  else
+    return TRUE
+  end
+  
+end
   
   
   
